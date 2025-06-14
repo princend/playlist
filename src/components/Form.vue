a8690c24035f86dd057271b01ec53a19879d3c79
Princend <waxdji3g4y3c04@gmail.com>
2025-06-14 19:28:43 +0800

    表單其他元件事件處理

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 
 let username = ref("");
+let gender = ref("");
 const handleInput = (e: { target: { value: any; }; }) => {
     console.log(e.target.value);
     username.value = e.target.value
@@ -10,12 +11,17 @@ const handleInput = (e: { target: { value: any; }; }) => {
 </script>
 
 <template>
-    <div>
+    <div style="color: blue;">
         <label for="username">用戶名</label>
         <input type="text" id="username" v-model="username">
+        <br>
+        性別:<input type="radio" name="gender" id="gender" value="male" v-model="gender">男
+        <input type="radio" name="gender" id="gender" value="female" v-model="gender">女
+
     </div>
     <div>
         <p style="color: blueviolet;">您輸入的用戶名是:{{ username }}</p>
+        <p style="color: blueviolet;">您的性別是:{{ gender }}</p>
     </div>
 
 </template>
