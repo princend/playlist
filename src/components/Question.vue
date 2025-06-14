c97c35b8158f7dc58bc6b7770a029408bf97cde6
Princend <waxdji3g4y3c04@gmail.com>
2025-06-14 19:02:48 +0800

    watch

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -6,17 +6,40 @@ const label = computed(() => {
    return  showAnswer.value ? '隱藏答案' : '顯示答案'
 })
 
-const toggleAnswer= ()=>{showAnswer.value=!showAnswer.value};
+const toggleAnswer = () => { showAnswer.value = !showAnswer.value };
 
-watch(showAnswer, (newVal, oldVal) => {
-  console.log('showAnswer 變了，新的值：', newVal, '舊的值：', oldVal);
+// 計時器 id
+let timer: ReturnType<typeof setTimeout> | null = null;
+let countdown = ref(0);
+
+watch(showAnswer, (newVal) => {
+  if (timer) {
+    clearTimeout(timer);
+    timer = null;
+  }
+  if (newVal) {
+    countdown.value = 5;
+    timer = setInterval(() => {
+      countdown.value--;
+      if (countdown.value <= 0) {
+        showAnswer.value = false;
+        clearInterval(timer!);
+        timer = null;
+      }
+    }, 1000);
+  } else {
+    countdown.value = 0;
+  }
 });
 </script>
 
 <template>
     <div class="question">
         <p>vue 是一個什麼樣的框架</p>
-        <p v-show="showAnswer">vue是一套用於建構用戶介面的漸進式框架</p>
+        <p v-show="showAnswer">vue是一套用於建構用戶介面的漸進式框架
+            <p v-if="showAnswer">{{ countdown }} 秒</p>
+        </p>
+        
 
         <button @click="toggleAnswer()">
             <span>
