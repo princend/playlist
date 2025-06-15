edcacb7f87afba98038f4a57d16e7601b1822cfb
Princend <waxdji3g4y3c04@gmail.com>
2025-06-15 15:24:35 +0800

    v-once

--- src/components/VHtml.vue
+++ src/components/VHtml.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 const content = `<p>這是一段html</p>`
+const list = ref([1, 2, 3])
 </script>
 <template>
-    <article class="text-blue" v-html="content"></article>
-
+    <div class="text-blue">
+        <article class="text-blue" v-html="content"></article>
+        <p v-once>list 初始長度 {{ list.length }}</p>
+    </div>
 </template>
