d4776a1f43c258d06551ad954d0146635049de24
Princend <waxdji3g4y3c04@gmail.com>
2025-06-15 19:16:22 +0800

    v-once

--- src/components/VHtml.vue
+++ src/components/VHtml.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const content = `<p>這是一段html</p>`
 const list = ref([1, 2, 3])
+
+
+const appendValue = computed(()=>{
+     list.value.push(list.value.length+1)
+})
 </script>
 <template>
     <div class="text-blue">
         <article class="text-blue" v-html="content"></article>
         <p v-once>list 初始長度 {{ list.length }}</p>
+        <button @click="() => appendValue"> 增加列表</button>
+        <div>list:{{ list }}</div>
+        
     </div>
 </template>
